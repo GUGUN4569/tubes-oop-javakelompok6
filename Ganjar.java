7f324b7e70064b08c6f2084fc6879c6b1207c256
GUGUN4569 <cahyaaris739@gmail.com>
2025-12-10 11:20:23 +0700

    Nimpa File Ganjar.java

--- Ganjar.java
+++ Ganjar.java
@@ -1,5 +1,5 @@
 public class Ganjar {
     public static void main(String[] args) {
-        System.out.println("Saya Ganjar Rizki Anugrah-1324059");
+        System.out.println("Saya Ganjar Rizki Aneugrah - 1324059");
     }
 }
